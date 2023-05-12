5372cb240129170c420a2101d29cfb2d3bd8d525
Davisoad <davibragaz18@gmail.com>
2023-05-12 11:30:09 -0300

    Projeto Entity Framework

---
+++ ExemploSession/Models/SeedData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExemploSession.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            Context context = app.ApplicationServices.GetRequiredService<Context>();
+            context.Database.Migrate();
+            if (!context.Usuarios.Any())
+            {
+                var usuario = new Usuario { Senha = "123456", Nome = "Mairon", Email = "davibragafast@gmail.com" };
+                context.Usuarios.AddRange(usuario);
+                context.SaveChanges();
+            }
+        }
+    }
+}
